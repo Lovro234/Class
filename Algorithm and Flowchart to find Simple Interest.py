1fa703726a98f74050cafc775df4e319f9fb54e1
Lovro234 <25LovroBatas.acc@lwetb.ie>
2026-03-24 15:03:57 +0000

    Add files via upload

---
+++ Algorithm and Flowchart to find Simple Interest.py
@@ -0,0 +1,5 @@
+P = float(input('Enter the Principal Amount:'))
+N = int(input('Enter the Time in Years:'))
+R = float(input('Enter the ARI:'))
+SI = (P*N*R)/100.0
+print('The simple interest is:',SI)
