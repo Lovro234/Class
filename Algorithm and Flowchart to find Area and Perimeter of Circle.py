be717c5aca663d1380764e787af6dc8efb31ae12
Lovro234 <25LovroBatas.acc@lwetb.ie>
2026-03-24 15:03:25 +0000

    Add files via upload

---
+++ Algorithm and Flowchart to find Area and Perimeter of Circle.py
@@ -0,0 +1,5 @@
+R = float(input('Enter the radius of the circle:'))
+Area = 22.0/7.0 * R * R
+PERIMETER = 2*22.0/7.0 * R
+print('The area of the circle is:',Area)
+print('The perimeter of the circle is:',PERIMETER)
